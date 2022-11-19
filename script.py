7b111ca91294f38d4770d659fbcb74caba5a24a9
bramha255 <51294522+bramha255@users.noreply.github.com>
2022-11-19 23:43:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,13 @@ st.header('Hello Page')
 
 if st.button('Balloon'):
   st.balloons()
-      
+  
+col1,col2 = st.columns(2)
+with col1:
+  st.write('LED')
+
+with col2:
+  radio=st.radio(label='radio', key='k1',label_visibility='collapsed', horizontal=True)
+
 
         
